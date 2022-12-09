d6ad67fb2b18c356144fec86cddea3b55087bb85
ebinaha <ebinha012@gmail.com>
2022-12-09 10:19:54 +0900

    Entity 상속 및 생성

--- src/main/java/com/example/jwt/entity/User.java
+++ src/main/java/com/example/jwt/entity/User.java
@@ -2,11 +2,37 @@ package com.example.jwt.entity;
 
 import java.util.Collection;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @AllArgsConstructor @NoArgsConstructor
 public class User implements UserDetails {
 
+	@Id
+	private String id;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String nickname;
+	
+	@Column
+	private String email;
+	
+	
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
@@ -16,37 +42,38 @@ public class User implements UserDetails {
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
-		return null;
+		return password;
 	}
 
+	//실제 유저네임이 아닌 PK (id)
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
